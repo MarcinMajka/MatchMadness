5c9aea00e7cf6f86cb7f3873dde70cbaacbfb294
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2024-08-07 20:09:33 +0200

    Bug fix: addex export to shuffleArray from utils.js

--- utils.js
+++ utils.js
@@ -4,7 +4,7 @@
  * @param array - an array of strings
  * @returns  a new array with the same strings, but in a random order
  */
-const shuffleArray = (array) => {
+export const shuffleArray = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
