cbc34eb32d67778ed2f0326daebb2f01ff85aee8
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2024-10-03 22:57:48 +0200

    #favWordsButton toggles using Liked Words

--- changePreferences.js
+++ changePreferences.js
@@ -21,6 +21,16 @@ favWordsButton.onclick = async function () {
       'currentSet',
       JSON.stringify(allFavWordsObjectsInArray)
     );
+    favWordsButton.classList.toggle('favWordsAdded');
+    if (favWordsButton.classList.contains('favWordsAdded')) {
+      favWordsButton.innerText = 'Yay Favorite Words!';
+    } else {
+      favWordsButton.innerText = 'Use Favorite Words!';
+      const currentSetNumber =
+        setIndexInput.value == '' ? 0 : setIndexInput.value - 1;
+      console.log(currentSetNumber);
+      initializeSet(currentSetNumber);
+    }
   } catch (error) {
     console.log(error);
   }
