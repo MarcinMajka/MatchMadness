fb68c3847a6afbfee4fc095c0089aaa088d96030
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2024-10-22 22:27:07 +0200

    Refactor: deleted commented out console logs from indexedDBHandler.js

--- indexedDBHandler.js
+++ indexedDBHandler.js
@@ -1,16 +1,13 @@
 export function openDatabase(dbName, storeName) {
   return new Promise((resolve, reject) => {
-    // console.log('Opening database:', dbName);
     const request = indexedDB.open(dbName, 1);
 
     request.onupgradeneeded = (event) => {
-      // console.log('Upgrading database:', dbName);
       const db = event.target.result;
       db.createObjectStore(storeName);
     };
 
     request.onsuccess = (event) => {
-      // console.log('Database opened successfully - dbName:', dbName);
       resolve(event.target.result);
     };
 
@@ -23,13 +20,11 @@ export function openDatabase(dbName, storeName) {
 
 export function saveToIndexedDB(db, storeName, key, data) {
   return new Promise((resolve, reject) => {
-    // console.log('Starting saving to IndexedDB:', key);
     const transaction = db.transaction(storeName, 'readwrite');
     const store = transaction.objectStore(storeName);
     const request = store.put(data, key);
 
     request.onsuccess = () => {
-      // console.log('Data saved to IndexedDB:', key);
       resolve();
     };
 
@@ -42,13 +37,11 @@ export function saveToIndexedDB(db, storeName, key, data) {
 
 export function getFromIndexedDB(db, storeName, key) {
   return new Promise((resolve, reject) => {
-    // console.log('Getting from IndexedDB:', key);
     const transaction = db.transaction(storeName, 'readonly');
     const store = transaction.objectStore(storeName);
     const request = store.get(key);
 
     request.onsuccess = (event) => {
-      // console.log('Data retrieved from IndexedDB:', key, event.target.result);
       resolve(event.target.result);
     };
 
@@ -61,7 +54,6 @@ export function getFromIndexedDB(db, storeName, key) {
 
 export async function fetchFile(url) {
   try {
-    // console.log('Fetching file:', url);
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error('Network response was not ok');
@@ -76,7 +68,6 @@ export async function fetchFile(url) {
 
 export async function loadDicIn50WordSets(dbName, storeName, url) {
   try {
-    // console.log('Loading dictionary in 50 word sets');
     const db = await openDatabase(dbName, storeName);
 
     // Check if data is already in IndexedDB
@@ -95,12 +86,10 @@ export async function loadDicIn50WordSets(dbName, storeName, url) {
 
       // Save the blob to IndexedDB
       await saveToIndexedDB(db, storeName, 'AllWordSetsBlob', fileData);
-      // console.log('File saved to IndexedDB');
     }
 
     // Convert Blob to JSON
     const text = await fileData.text();
-    // console.log('File content:', text); // Log the content of the file
     const jsonData = JSON.parse(text);
 
     // Check if AllWordSetsBlob exists and is an array
@@ -108,7 +97,6 @@ export async function loadDicIn50WordSets(dbName, storeName, url) {
       throw new Error('Data is not an array');
     }
 
-    // console.log('Successfully loaded AllWordSetsBlob');
     return jsonData;
   } catch (error) {
     console.error('Error loading AllWordSetsBlob:', error);
