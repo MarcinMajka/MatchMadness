d5ac035a139dd0a888a2d22bdba2c1ed2710e373
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2024-10-23 14:38:20 +0200

    Refactor: merged defaultPreferences.js and changePreferences.js

--- defaultPreferences.js
+++ defaultPreferences.js
@@ -1,4 +1,5 @@
 import { loadDicIn50WordSets } from './indexedDBHandler.js';
+import { getAllWords } from './favoriteWords.js';
 
 const DB_NAME = 'AllWordSets';
 const STORE_NAME = 'WordSets';
@@ -12,7 +13,7 @@ const parseAsTriplets = (listOfTripletsData) => {
   }));
 };
 
-export async function initializeSet(setIndex) {
+async function initializeSet(setIndex) {
   try {
     const wordSets = await loadDicIn50WordSets(DB_NAME, STORE_NAME, FILE_URL);
     const currentSet = parseAsTriplets(wordSets[setIndex]);
@@ -22,5 +23,44 @@ export async function initializeSet(setIndex) {
     console.error('Failed to initialize data:', error);
   }
 }
+
+const setIndexInput = document.getElementById('setIndexInput');
+const setPairsToRenderInput = document.getElementById('setPairsToRenderInput');
+const favWordsButton = document.getElementById('favWordsButton');
+setIndexInput.addEventListener('input', (event) => {
+  const input = event.target.value;
+  const index = input ? Number(input) - 1 : 0;
+  initializeSet(index);
+});
+setPairsToRenderInput.addEventListener('input', (event) => {
+  localStorage.setItem('pairsToRender', event.target.value);
+});
+favWordsButton.onclick = async function () {
+  try {
+    const allFavWords = await getAllWords();
+
+    if (allFavWords.length === 0) {
+      alert("You don't have any favorite words yet. Like some words first!");
+      return;
+    }
+
+    const allFavWordsObjectsInArray = [...allFavWords];
+    localStorage.setItem(
+      'currentSet',
+      JSON.stringify(allFavWordsObjectsInArray)
+    );
+    favWordsButton.classList.toggle('favWordsAdded');
+    if (favWordsButton.classList.contains('favWordsAdded')) {
+      favWordsButton.innerText = 'Yay Favorite Words!';
+    } else {
+      favWordsButton.innerText = 'Use Favorite Words!';
+      const currentSetNumber =
+        setIndexInput.value == '' ? 0 : setIndexInput.value - 1;
+      initializeSet(currentSetNumber);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 initializeSet(0);
