444df311f3e74ffe32c363931f818c7035b5c9b5
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2024-11-30 23:04:37 +0100

    Geneeralised Word, Hiragana and Katakana Madness

---
+++ wordGame.js
@@ -0,0 +1,149 @@
+import { getElement, createUIElement } from './wrappers.js';
+import { toRomaji, toKatakana } from './node_modules/wanakana/esm/index.js';
+import { displayHint } from './utils.js';
+
+class WordGame {
+  constructor(config) {
+    // Configuration options with defaults
+    this.config = {
+      gameType: 'kanji',
+      hintFunction: displayHint,
+      nextButtonId: 'testNext',
+      ...config,
+    };
+
+    // DOM Element References
+    this.elements = {
+      word: getElement('#word'),
+      input: getElement('#userInput'),
+      glossary: getElement('#glossary'),
+      matches: getElement('#matches'),
+      wrong: getElement('#wrong'),
+    };
+
+    // Game State
+    this.data = JSON.parse(localStorage.getItem('currentSet'));
+    this.wordIndex = 0;
+    this.currentSetMatchCount = 0;
+    this.wrongCountInSet = 0;
+
+    // Bind methods to maintain context
+    this.validateInput = this.validateInput.bind(this);
+    this.updateWord = this.updateWord.bind(this);
+
+    this.setupEventListeners();
+  }
+
+  setupEventListeners() {
+    // Initial game setup
+    window.addEventListener('load', () => {
+      this.updateWord();
+      this.elements.input.focus();
+      this.displayMatches();
+      this.displayFailedTries();
+    });
+
+    // Optional next button
+    const nextButton = document.getElementById(this.config.nextButtonId);
+    if (nextButton) {
+      nextButton.onclick = this.updateWord;
+    }
+  }
+
+  validateInput(e) {
+    if (e.code === 'Space' || e.code === 'Enter') {
+      const currentInput = getElement('#userInput');
+      const inputValue = currentInput.value.trim();
+
+      // Skip empty inputs
+      if (inputValue === '') return;
+
+      const currentWord = this.data[this.wordIndex - 1];
+      const inputMatchesReading = inputValue === toRomaji(currentWord.reading);
+
+      if (inputMatchesReading) {
+        this.currentSetMatchCount++;
+        this.updateGlossary();
+        this.updateWord();
+      } else {
+        this.config.hintFunction(currentInput, this.data, this.wordIndex);
+        this.wrongCountInSet++;
+        this.displayFailedTries();
+      }
+    }
+  }
+
+  updateWord() {
+    this.displayMatches();
+    console.log('Game Type:', this.config.gameType);
+
+    if (this.wordIndex < this.data.length) {
+      const currentWord = this.data[this.wordIndex];
+      const displayValue =
+        this.config.gameType === 'kanji'
+          ? currentWord.kanji
+          : this.config.gameType === 'katakana'
+          ? toKatakana(currentWord.reading)
+          : currentWord.reading;
+
+      this.elements.word.innerText = displayValue;
+
+      // Rest of the method remains the same
+      const newInput = createUIElement('input');
+      newInput.type = 'text';
+      newInput.id = 'userInput';
+
+      newInput.addEventListener('keydown', (e) => {
+        if (e.code === 'Space') {
+          e.preventDefault();
+        }
+      });
+
+      newInput.addEventListener('keyup', this.validateInput);
+
+      getElement('#userInput').replaceWith(newInput);
+      newInput.focus();
+
+      this.wordIndex++;
+    } else {
+      // Reset or complete game logic
+      this.wordIndex = 0;
+      this.currentSetMatchCount = 0;
+
+      if (this.wordIndex === this.data.length) {
+        this.updateWord();
+      } else {
+        alert('Congrats!');
+      }
+    }
+  }
+
+  updateGlossary() {
+    const currentWord = this.data[this.wordIndex - 1];
+    this.elements.glossary.innerText = `${currentWord.kanji} - ${currentWord.reading}\n${currentWord.glossary}`;
+  }
+
+  displayMatches() {
+    const currentSetLength = this.data.length;
+    this.elements.matches.innerText = `${this.currentSetMatchCount}/${currentSetLength}`;
+  }
+
+  displayFailedTries() {
+    this.elements.wrong.innerText = `Fails: ${this.wrongCountInSet}`;
+  }
+}
+
+// Updated game variations
+const KanjiToReadingGame = new WordGame({
+  gameType: 'kanji',
+});
+
+const ReadingToRomajiGame = new WordGame({
+  gameType: 'reading',
+});
+
+const ReadingToKatakanaGame = new WordGame({
+  gameType: 'katakana',
+});
+
+export { WordGame };
--- wordMadness.js
+++ wordMadness.js
@@ -1,98 +1,104 @@
-import { displayHint } from './utils.js';
-import { getElement, createUIElement } from './wrappers.js';
-import { toRomaji } from './node_modules/wanakana/esm/index.js';
+// import { displayHint } from './utils.js';
+// import { getElement, createUIElement } from './wrappers.js';
+// import { toRomaji } from './node_modules/wanakana/esm/index.js';
 
-const wordMadnessWord = getElement('#word');
-const wordMadnessInput = getElement('#userInput');
-const wordMadnessGlossary = getElement('#glossary');
-const matchesInSetButton = getElement('#matches');
-const wrongCountInSetButton = getElement('#wrong');
+// const wordMadnessWord = getElement('#word');
+// const wordMadnessInput = getElement('#userInput');
+// const wordMadnessGlossary = getElement('#glossary');
+// const matchesInSetButton = getElement('#matches');
+// const wrongCountInSetButton = getElement('#wrong');
 
-const data = JSON.parse(localStorage.getItem('currentSet'));
+// const data = JSON.parse(localStorage.getItem('currentSet'));
 
-let wordIndex = 0;
-let currentSetMatchCount = 0;
-let wrongCountInSet = 0;
+// let wordIndex = 0;
+// let currentSetMatchCount = 0;
+// let wrongCountInSet = 0;
 
-function validateInput(e) {
-  // Get fresh reference to input element since it may have been replaced
-  const currentInput = getElement('#userInput');
+// function validateInput(e) {
+//   // Get fresh reference to input element since it may have been replaced
+//   const currentInput = getElement('#userInput');
 
-  if (e.code === 'Space' || e.code === 'Enter') {
-    const inputValue = currentInput.value.trim();
-    const currentWord = data[wordIndex - 1];
-    const inputMatchesReading = inputValue === toRomaji(currentWord.reading);
+//   if (e.code === 'Space' || e.code === 'Enter') {
+//     const inputValue = currentInput.value.trim();
+//     const currentWord = data[wordIndex - 1];
+//     const inputMatchesReading = inputValue === toRomaji(currentWord.reading);
 
-    if (inputMatchesReading) {
-      currentSetMatchCount++;
-      updateGlossary();
-      updateWord();
-    } else {
-      // If input is blank, let's not count it towards fails
-      if (inputValue === '') return;
-      displayHint(currentInput, data, wordIndex);
-      wrongCountInSet++;
-      displayFailedTries();
-    }
-  }
-}
+//     if (inputMatchesReading) {
+//       currentSetMatchCount++;
+//       updateGlossary();
+//       updateWord();
+//     } else {
+//       // If input is blank, let's not count it towards fails
+//       if (inputValue === '') return;
+//       displayHint(currentInput, data, wordIndex);
+//       wrongCountInSet++;
+//       displayFailedTries();
+//     }
+//   }
+// }
 
-function updateWord() {
-  console.log(data[wordIndex]);
-  displayMatches();
-  if (wordIndex < data.length) {
-    wordMadnessWord.innerText = data[wordIndex].kanji;
+// function updateWord() {
+//   console.log(data[wordIndex]);
+//   displayMatches();
+//   if (wordIndex < data.length) {
+//     wordMadnessWord.innerText = data[wordIndex].kanji;
 
-    // Create fresh input element to fix IME (Input Method Editor) persistence issues
-    // This prevents Japanese input method from reappearing when it should be cleared
-    const newInput = createUIElement('input');
-    newInput.type = 'text';
-    newInput.id = 'userInput';
+//     // Create fresh input element to fix IME (Input Method Editor) persistence issues
+//     // This prevents Japanese input method from reappearing when it should be cleared
+//     const newInput = createUIElement('input');
+//     newInput.type = 'text';
+//     newInput.id = 'userInput';
 
-    // Add event listeners to new input since they don't carry over from old element
-    newInput.addEventListener('keyup', validateInput);
-    newInput.addEventListener('keydown', (e) => {
-      if (e.code === 'Space') {
-        e.preventDefault();
-      }
-    });
+//     // Add event listeners to new input since they don't carry over from old element
+//     newInput.addEventListener('keyup', validateInput);
+//     newInput.addEventListener('keydown', (e) => {
+//       if (e.code === 'Space') {
+//         e.preventDefault();
+//       }
+//     });
 
-    // Replace old input with new one while maintaining exact position in DOM
-    getElement('#userInput').replaceWith(newInput);
+//     // Replace old input with new one while maintaining exact position in DOM
+//     getElement('#userInput').replaceWith(newInput);
 
-    // Focus new input to allow immediate typing
-    newInput.focus();
+//     // Focus new input to allow immediate typing
+//     newInput.focus();
 
-    wordIndex++;
-  } else {
-    wordIndex = 0;
-    currentSetMatchCount = 0;
-    if (wordIndex === data.length) {
-      updateWord();
-    } else {
-      alert('Congrats!');
-    }
-  }
-}
+//     wordIndex++;
+//   } else {
+//     wordIndex = 0;
+//     currentSetMatchCount = 0;
+//     if (wordIndex === data.length) {
+//       updateWord();
+//     } else {
+//       alert('Congrats!');
+//     }
+//   }
+// }
 
-function updateGlossary() {
-  wordMadnessGlossary.innerText = `${data[wordIndex - 1].kanji} - ${
-    data[wordIndex - 1].reading
-  }\n ${data[wordIndex - 1].glossary}`;
-}
+// function updateGlossary() {
+//   wordMadnessGlossary.innerText = `${data[wordIndex - 1].kanji} - ${
+//     data[wordIndex - 1].reading
+//   }\n ${data[wordIndex - 1].glossary}`;
+// }
 
-function displayMatches() {
-  const currentSetLenght = data.length;
-  matchesInSetButton.innerText = `${currentSetMatchCount}/${currentSetLenght}`;
-}
+// function displayMatches() {
+//   const currentSetLenght = data.length;
+//   matchesInSetButton.innerText = `${currentSetMatchCount}/${currentSetLenght}`;
+// }
 
-function displayFailedTries() {
-  wrongCountInSetButton.innerText = `Fails: ${wrongCountInSet}`;
-}
+// function displayFailedTries() {
+//   wrongCountInSetButton.innerText = `Fails: ${wrongCountInSet}`;
+// }
 
-window.addEventListener('load', () => {
-  updateWord();
-  wordMadnessInput.focus();
-  displayMatches();
-  displayFailedTries();
+// window.addEventListener('load', () => {
+//   updateWord();
+//   wordMadnessInput.focus();
+//   displayMatches();
+//   displayFailedTries();
+// });
+
+import { WordGame } from './wordGame.js';
+
+const KanjiToReadingGame = new WordGame({
+  gameType: 'kanji',
 });
