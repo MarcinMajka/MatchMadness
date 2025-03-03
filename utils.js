4cf57aeabc38097070465aa2588bd7edd17691f0
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2025-03-03 21:19:34 +0100

    utils.js: added removeClass()

--- utils.js
+++ utils.js
@@ -116,6 +116,10 @@ export const addClass = (element, className) => {
   element.classList.add(className);
 };
 
+export const removeClass = (element, className) => {
+  element.classList.remove(className);
+};
+
 // module.exports = {
 //   shuffleArray,
 // };
