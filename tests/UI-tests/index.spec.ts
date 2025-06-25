7efe8195f83d0529c6e95fc63e935a1e34477f17
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2025-06-25 23:53:16 +0200

    Added a second test

--- tests/UI-tests/index.spec.ts
+++ tests/UI-tests/index.spec.ts
@@ -1,6 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('Checks for buttons', async ({ page }) => {
   await page.goto('http://127.0.0.1:5500/');
   await expect(page.locator('.menuButton').first()).toHaveText('Match Madness');
+});
+
+test('Checks for menu buttons number', async ({ page }) => {
+  await page.goto('http://127.0.0.1:5500/');
+  const menuButtons = page.locator('.menuButton');
+  await expect(menuButtons).toHaveCount(7);
 });
