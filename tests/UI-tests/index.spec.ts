b76aad8ddf3e8297f90f0e26a06d5d2c527f6112
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2025-06-30 14:13:04 +0200

    Added a timeout for fetching word sets - fixing rare Chromium flakiness on pairs tests

--- tests/UI-tests/index.spec.ts
+++ tests/UI-tests/index.spec.ts
@@ -14,6 +14,9 @@ test('Checks for menu buttons number', async ({ page }) => {
 // Doesn't work on webkit
 test('Checks minimum pairs per screen', async ({ page }) => {
   await page.goto('http://127.0.0.1:5500/');
+  // Needed for fetching kanjis from dicIn50WordSets.json
+  await page.waitForTimeout(100);
+  
   const pairInput = page.locator('#setPairsToRenderInput')
   const leftColumn = page.locator('.leftColumn');
   const rightColumn = page.locator('.rightColumn');
@@ -21,6 +24,9 @@ test('Checks minimum pairs per screen', async ({ page }) => {
   for (let i = 0; i <= 2; i++) {
     await pairInput.fill(i.toString());
     await page.click('.menuButton:has-text("Match Madness")');
+
+    
+
     if (i >= 0 && i <= 3) {
       await expect(leftColumn.locator('.box')).toHaveCount(3);
       await expect(rightColumn.locator('.box')).toHaveCount(3);
@@ -31,6 +37,9 @@ test('Checks minimum pairs per screen', async ({ page }) => {
 
 test('Checks maximum pairs per screen', async ({ page }) => {
   await page.goto('http://127.0.0.1:5500/');
+  // Needed for fetching kanjis from dicIn50WordSets.json
+  await page.waitForTimeout(100);
+  
   const pairInput = page.locator('#setPairsToRenderInput')
   const leftColumn = page.locator('.leftColumn');
   const rightColumn = page.locator('.rightColumn');
@@ -38,6 +47,8 @@ test('Checks maximum pairs per screen', async ({ page }) => {
   for (let i = 10; i >= 7; i--) {
     await pairInput.fill(i.toString());
     await page.click('.menuButton:has-text("Match Madness")');
+
+    
     
     await expect(leftColumn.locator('.box')).toHaveCount(7);
     await expect(rightColumn.locator('.box')).toHaveCount(7);
