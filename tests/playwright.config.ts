a5223aa10b265f38ef17674628e3035153da1152
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2025-06-30 13:19:44 +0200

    Commented out webkit from playwright.config

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
@@ -44,10 +45,10 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
