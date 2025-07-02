01aad1a606bb2cb853214d42f37e8a235d9c47fc
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2025-07-03 00:31:39 +0200

    PW: bug fix - made better locators for each menu button

--- pages/index.page.ts
+++ pages/index.page.ts
@@ -18,11 +18,11 @@ export class IndexPage {
         this.pairInput = page.locator('#setPairsToRenderInput');
         this.setInput = page.locator('#setIndexInput');
         this.menuButtons = page.locator('.menuButton');
-        this.matchMadnessButton = this.menuButtons.locator(':has-text("Match Madness")');
-        this.wordMadnessButton = this.menuButtons.locator(':has-text("Word Madness")');
-        this.hiraganaMadnessButton = this.menuButtons.locator(':has-text("Hiragana Madness")');
-        this.katakanaMadnessButton = this.menuButtons.locator(':has-text("Katakana Madness")');
-        this.memoryMadnessButton = this.menuButtons.locator(':has-text("Memory Madness")');
+        this.matchMadnessButton = page.locator('a.menuButton[href="matchMadness.html"]');
+        this.wordMadnessButton = page.locator('a.menuButton[href="wordMadness.html"]');
+        this.hiraganaMadnessButton = page.locator('a.menuButton[href="hiraganaMadness.html"]');
+        this.katakanaMadnessButton = page.locator('a.menuButton[href="katakanaMadness.html"]');
+        this.memoryMadnessButton = page.locator('a.menuButton[href="memoryMadness.html"]');
         this.addFavWordsButton = this.menuButtons.locator(':has-text("Add Fav Words")');
         this.useFavWordsButton = this.menuButtons.locator(':has-text("Use Favorite Words!")');
     }
