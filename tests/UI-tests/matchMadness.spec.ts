a60891e15e8c1948340ba949c7ca570a6d57bbfa
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2025-07-02 16:23:26 +0200

    Used POM in matchMadness.spec.ts

--- tests/UI-tests/matchMadness.spec.ts
+++ tests/UI-tests/matchMadness.spec.ts
@@ -1,7 +1,11 @@
 import { test, expect } from '@playwright/test';
+import { MatchMadness } from '../../pages/matchMadness.page';
 
 test.describe('User selects number of pairs to display per screen', () => {
+  let matchMadnessPage: MatchMadness;
+
   test.beforeEach(async ({ page }) => {
+    matchMadnessPage = new MatchMadness(page);
     await page.goto('/');
     // Needed for fetching kanjis from dicIn50WordSets.json
     await page.waitForTimeout(100);
@@ -9,49 +13,37 @@ test.describe('User selects number of pairs to display per screen', () => {
 
   // Doesn't work on webkit
   test('Minimum pairs', async ({ page }) => {
-    const pairInput = page.locator('#setPairsToRenderInput')
-    const leftColumn = page.locator('.leftColumn');
-    const rightColumn = page.locator('.rightColumn');
-  
     for (let i = 0; i <= 2; i++) {
-      await pairInput.fill(i.toString());
+      await matchMadnessPage.pairInput.fill(i.toString());
       await page.click('.menuButton:has-text("Match Madness")');
   
       if (i >= 0 && i <= 3) {
-        await expect(leftColumn.locator('.box')).toHaveCount(3);
-        await expect(rightColumn.locator('.box')).toHaveCount(3);
+        await expect(matchMadnessPage.leftColumn.locator('.box')).toHaveCount(3);
+        await expect(matchMadnessPage.rightColumn.locator('.box')).toHaveCount(3);
       }
       await page.click('a.button.menu');
     }
   });
   
   test('Maximum pairs', async ({ page }) => {
-    const pairInput = page.locator('#setPairsToRenderInput')
-    const leftColumn = page.locator('.leftColumn');
-    const rightColumn = page.locator('.rightColumn');
-  
     for (let i = 10; i >= 7; i--) {
-      await pairInput.fill(i.toString());
+      await matchMadnessPage.pairInput.fill(i.toString());
       await page.click('.menuButton:has-text("Match Madness")');
   
-      await expect(leftColumn.locator('.box')).toHaveCount(7);
-      await expect(rightColumn.locator('.box')).toHaveCount(7);
+      await expect(matchMadnessPage.leftColumn.locator('.box')).toHaveCount(7);
+      await expect(matchMadnessPage.rightColumn.locator('.box')).toHaveCount(7);
   
       await page.click('a.button.menu');
     }
   });
   
   test('Valid pairs', async ({ page }) => {
-    const pairInput = page.locator('#setPairsToRenderInput')
-    const leftColumn = page.locator('.leftColumn');
-    const rightColumn = page.locator('.rightColumn');
-  
     for (let i = 4; i <= 6; i++) {
-      await pairInput.fill(i.toString());
+      await matchMadnessPage.pairInput.fill(i.toString());
       await page.click('.menuButton:has-text("Match Madness")');
       
-      await expect(leftColumn.locator('.box')).toHaveCount(i);
-      await expect(rightColumn.locator('.box')).toHaveCount(i);
+      await expect(matchMadnessPage.leftColumn.locator('.box')).toHaveCount(i);
+      await expect(matchMadnessPage.rightColumn.locator('.box')).toHaveCount(i);
   
       await page.click('a.button.menu');
     }
