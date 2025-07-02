d9ec45bc337513ac978eb911a8fba744d5329748
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2025-07-02 23:18:29 +0200

    Added tests for the rest of the index.html buttons

--- tests/UI-tests/index.spec.ts
+++ tests/UI-tests/index.spec.ts
@@ -9,11 +9,35 @@ test.describe('Checks for menu buttons', () => {
     await page.goto('/');
   });
 
-  test('Checks for buttons', async ({ page }) => {
+  test('Checks for menu buttons number', async ({ page }) => {
+    await expect(indexPage.menuButtons).toHaveCount(7);
+  });
+
+  test('Checks for Match Madness', async ({ page }) => {
     await expect(indexPage.menuButtons.first()).toHaveText('Match Madness');
   });
 
-  test('Checks for menu buttons number', async ({ page }) => {
-    await expect(indexPage.menuButtons).toHaveCount(7);
+  test('Checks for Word Madness', async ({ page }) => {
+    await expect(indexPage.menuButtons.nth(1)).toHaveText('Word Madness');
+  });
+
+  test('Checks for Hiragana Madness', async ({ page }) => {
+    await expect(indexPage.menuButtons.nth(2)).toHaveText('Hiragana Madness');
+  });
+
+  test('Checks for Katakana Madness', async ({ page }) => {
+    await expect(indexPage.menuButtons.nth(3)).toHaveText('Katakana Madness');
+  });
+
+  test('Checks for Memory Madness', async ({ page }) => {
+    await expect(indexPage.menuButtons.nth(4)).toHaveText('Memory Madness');
+  });
+
+  test('Checks for Add Fav Words', async ({ page }) => {
+    await expect(indexPage.menuButtons.nth(5)).toHaveText('Add Fav Words');
+  });
+
+  test('Checks for Use Favorite Words!', async ({ page }) => {
+    await expect(indexPage.menuButtons.nth(6)).toHaveText('Use Favorite Words!');
   });
 });
