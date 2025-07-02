c023b513133099639fe78931c0eee337ab599ef6
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2025-07-02 16:35:23 +0200

    Made IndexPage

---
+++ pages/index.page.ts
@@ -0,0 +1,25 @@
+import { Page, Locator } from '@playwright/test';
+
+export class IndexPage {
+    page: Page;
+    menuButtons: Locator;
+    matchMadnessButton: Locator;
+    wordMadnessButton: Locator;
+    hiraganaMadnessButton: Locator;
+    katakanaMadnessButton: Locator;
+    memoryMadnessButton: Locator;
+    addFavWordsButton: Locator;
+    useFavWordsButton: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.menuButtons = page.locator('.menuButton');
+        this.matchMadnessButton = this.menuButtons.locator(':has-text("Match Madness")');
+        this.wordMadnessButton = this.menuButtons.locator(':has-text("Word Madness")');
+        this.hiraganaMadnessButton = this.menuButtons.locator(':has-text("Hiragana Madness")');
+        this.katakanaMadnessButton = this.menuButtons.locator(':has-text("Katakana Madness")');
+        this.memoryMadnessButton = this.menuButtons.locator(':has-text("Memory Madness")');
+        this.addFavWordsButton = this.menuButtons.locator(':has-text("Add Fav Words")');
+        this.useFavWordsButton = this.menuButtons.locator(':has-text("Use Favorite Words!")');
+    }
+}
