9663a3abcf2e3f6110e4e446ae7c2696790ef0dc
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2025-07-06 23:51:44 +0200

    Extracted a new test.describe and added a TODO

--- tests/UI-tests/matchMadness.spec.ts
+++ tests/UI-tests/matchMadness.spec.ts
@@ -5,7 +5,6 @@ import { IndexPage } from '../../pages/index.page';
 test.describe('User selects number of pairs to display per screen', () => {
   let indexPage: IndexPage;
   let matchMadnessPage: MatchMadness;
-  let currentSet: Array<Object>;
 
   test.beforeEach(async ({ page }) => {
     indexPage = new IndexPage(page);
@@ -19,13 +18,8 @@ test.describe('User selects number of pairs to display per screen', () => {
   test('Minimum pairs', async ({ page }) => {
     for (let i = 0; i <= 2; i++) {
       await indexPage.pairInput.fill(i.toString());
-      await page.click('.menuButton:has-text("Match Madness")');
-
-      // Handle the option of getItem() returing an error
-      currentSet = await page.evaluate(() => {
-        const item = localStorage.getItem('currentSet');
-        return item ? JSON.parse(item) : [];
-      });
+      await indexPage.matchMadnessButton.click();
+      // await page.click('.menuButton:has-text("Match Madness")');
 
       if (i >= 0 && i <= 3) {
         await expect(matchMadnessPage.leftColumn.locator('.box')).toHaveCount(3);
@@ -58,4 +52,35 @@ test.describe('User selects number of pairs to display per screen', () => {
       await matchMadnessPage.menuButton.click();
     }
   });
+});
+
+// TODO: make a test that takes first .box value from the left, looks for correct hiragana value and selects the correct .box from the right
+test.describe('Playing the game', () => {
+  let indexPage: IndexPage;
+  let matchMadnessPage: MatchMadness;
+  let currentSet: Array<Object>;
+
+  test.beforeEach(async ({ page }) => {
+    indexPage = new IndexPage(page);
+    matchMadnessPage = new MatchMadness(page);
+    await page.goto('/');
+    // Needed for fetching kanjis from dicIn50WordSets.json
+    await page.waitForTimeout(100);
+
+    indexPage.matchMadnessButton.click();    
+
+    // Handle the option of getItem() returing an error
+    currentSet = await page.evaluate(() => {
+      const item = localStorage.getItem('currentSet');
+      return item ? JSON.parse(item) : [];
+    });
+  });
+
+  // test('Selects correct answers only', ({ page }) => {
+  //   const currentWords = matchMadnessPage.leftColumn.locator('.box');
+  //   console.log(currentWords);
+  //   // for (const word of currentWords) {
+
+  //   // }
+  // });
 });
